9649fee1a3a90ee467db60447fe95514dd8f7204
surferzer <mafernandazr@gmail.com>
2019-08-22 18:24:35 -0500

    valida links pero solo poniendo el link como string

--- index.js
+++ index.js
@@ -43,20 +43,90 @@ convertMd(readMd('./README.md'));
 //BUSCAR LINKS EN EL HTML
 const cheerio = require ('cheerio');
 
-let htmlMd = convertMd(readMd('./README.md'));
-
+const htmlMd = convertMd(readMd('./README.md'));
+//let fileUrl="";
 const findLinksHtml=(htmlMd) => {
-  if((typeof findLinksHtml !== 'undefined')){
+ // if((typeof htmlMd !== 'undefined')){
       let $ = cheerio.load(htmlMd);
       let anchorTag = $('a');
       //console.log(anchorTag);
       // Links
       $(anchorTag).each((i, a) =>{
          let fileUrl= $(a).attr('href');
-         console.log(fileUrl);
+         return console.log(fileUrl.toString());
+         //console.log(fileUrl);
       })
-    }else{
-      console.log('error');
-    };
+  //  linkList(fileUrl);
+   // }else{
+   //  console.log('este archivo no es un html');
+   // };
+ 
 };
-findLinksHtml(htmlMd);
+
+
+
+const resultado = findLinksHtml(htmlMd)
+
+
+
+
+//VALIDACION DE LINKS
+
+//intento #1
+//let thereAreLinks = findLinksHtml(htmlMd);
+/*const http = require('http')
+function getStatusCodeResult(website) {
+  return new Promise((resolve, reject) => {
+      http.get(website, (res) => {
+          let statusCode = res.statusCode,
+              error = statusCode >= 400 && statusCode <= 500 ? `error: ${website}`: null
+          if (error) {
+              reject(error)
+          } else if (statusCode >= 200 && statusCode <= 300) {
+              resolve(`Success: ${website}`)
+            }
+          }, 0)
+      })
+};
+
+// LOOP PROMISES
+const getAllStatusCodeResult = (websites) => {
+  website.forEach((website) => {
+      getStatusCodeResult(website)
+          .then((result) => {
+              console.log(result)
+          })
+          .catch(error => {
+              console.log('error', error)
+          })
+  })
+}
+getAllStatusCodeResult(thereAreLinks); */
+
+//intento #2 con axios
+
+const axios = require ('axios');
+
+ const linkList=()=>{ 
+
+  //let resultado =  findLinksHtml(htmlMd);
+  console.log("ESTÁ MANDANDO ESTA DATA" , resultado);
+
+ axios.get('http://www.cannabisyyis.net') 
+  .then((data) => console.log((data.status) + " OK " + " ESTE LINK ESTA FUNCIONANDO" ))
+
+  .catch(function (error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+     
+      console.log(error.response.status);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+     console.log("404 BROKEN LINK " + '\n'+error.message);
+    }
+    console.log(error.config);
+  }
+  )};
+
+  console.log(linkList());
